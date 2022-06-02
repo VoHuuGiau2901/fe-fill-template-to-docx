073e89bbc25a8c06463df1a5f67a9f71c97f9da6
VoHuuGiau2901 <vohuugiau2901@gmail.com>
2022-06-02 21:39:18 +0700

    hehe
    hehe

---
+++ src/App.js
@@ -0,0 +1,96 @@
+import './App.css';
+import './Color.css';
+import React from 'react';
+import FingerPrint_img from './image/FG.png';
+import Logo_img from './image/LOGO-TRUNG-TAM-NEW.png';
+import {getServerLink} from './Link';
+
+function App() {
+
+  // const {type,setType}= useState("");
+
+  const handleClick = (value) => {
+    let url = getServerLink() + value;
+    // console.log(url);
+    window.open(url)
+  }
+
+  return (
+    <div>
+      <div className='container left'>
+        <a href='https://mentoring.edu.vn/?fbclid=IwAR3BENiyRYrI-BTRutMfAOYdXLdWiJZCzExgXOmgFwJkmV2x5jds1rhJRWI' alt="#" target="_blank" rel="noreferrer">
+          <figure className='wrapper_img'>
+            <img src={Logo_img} className="img_left" alt="#" />
+          </figure>
+        </a>
+      </div>
+      <div className='container right'>
+        <div className='serviceBox'  onClick={e => handleClick("UL")}>
+          <div className='icon ic1'>
+            <img src={FingerPrint_img} className="img" alt="#" />UL
+          </div>
+          <div className='content'>
+            <h2>Linh Động</h2>
+            <p>Ôn hòa, thân thiện, cởi mở, lãng mạn</p>
+          </div>
+        </div>
+        <div className='serviceBox'  onClick={e => handleClick("RL")}>
+          <div className='icon ic2'>
+            <img src={FingerPrint_img} className="img" alt="#" />RL
+          </div>
+          <div className='content'>
+            <h2>Thích Suy Luận</h2>
+            <p>Linh động, mạnh mẽ</p>
+          </div>
+        </div>
+        <div className='serviceBox'  onClick={e => handleClick("WC")}>
+          <div className='icon ic3'>
+            <img src={FingerPrint_img} className="img" alt="#" />WC – WD - WI
+          </div>
+          <div className='content'>
+            <h2>Hướng Ngoại</h2>
+            <p>Đa mục tiêu, nhìn nhận vấn đề đa chiều</p>
+          </div>
+        </div>
+        <div className='serviceBox'  onClick={e => handleClick("WP")}>
+          <div className='icon ic4'>
+            <img src={FingerPrint_img} className="img" alt="#" />WP - WL
+          </div>
+          <div className='content'>
+            <h2>Sáng Tạo</h2>
+            <p>Chủ nghĩa hoàn hảo</p>
+          </div>
+        </div>
+        <div className='serviceBox'  onClick={e => handleClick("WT")}>
+          <div className='icon ic5'>
+            <img src={FingerPrint_img} className="img" alt="#" />WT - WS
+          </div>
+          <div className='content'>
+            <h2>Lãnh Đạo Tốt</h2>
+            <p>Lập mục tiêu rõ ràng để hành động</p>
+          </div>
+        </div>
+        <div className='serviceBox'  onClick={e => handleClick("WE")}>
+          <div className='icon ic6'>
+            <img src={FingerPrint_img} className="img" alt="#" />WE
+          </div>
+          <div className='content'>
+            <h2>Sâu Sắc</h2>
+            <p>Có tầm nhìn xa</p>
+          </div>
+        </div>
+        <div className='serviceBox'  onClick={e => handleClick("ARCH")}>
+          <div className='icon ic7'>
+            <img src={FingerPrint_img} className="img" alt="#" />ARCH
+          </div>
+          <div className='content'>
+            <h2>Ham học hỏi</h2>
+            <p>Cẩn thận, làm việc có trình tự</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Link.js
@@ -0,0 +1,3 @@
+export function getServerLink(){
+    return "https://fill-template-nodejs-backend.herokuapp.com/";
+}
